dbf630d18203034279793546b67c8854c56a00c8
Yuri Nogueira <ynogueira317@gmail.com>
2024-04-04 22:37:29 -0300

    criando os templates

--- setup/urls.py
+++ setup/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.urls import path
 
-from mycontacts.views import home
 
-urlpatterns = [path("admin/", admin.site.urls), path("", home)]
+urlpatterns = [path("admin/", admin.site.urls), ]
